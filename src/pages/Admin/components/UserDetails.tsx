8855015554aa6586a8268c2c7dbdcd6f358493a5
Gustavonn07 <gustavonepnog@gmail.com>
2025-02-13 17:08:32 -0300

    style: details user

--- src/pages/Admin/components/UserDetails.tsx
+++ src/pages/Admin/components/UserDetails.tsx
@@ -2,6 +2,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components";
 import { useGetUser } from "../hooks/useApi";
 import { Dispatch, SetStateAction, useEffect } from "react";
 import { getRoleText } from "@/types/roleType";
+import { format } from "date-fns";
 
 export const UserDetails = ({
   id,
@@ -19,32 +20,34 @@ export const UserDetails = ({
 
     console.log("Requisição para ID:", id);
     getUser.makeRequest({ id });
-    
   }, [open, id]);
 
   return (
     <Dialog onOpenChange={setOpen} open={open}>
-      <DialogContent className="">
+      <DialogContent className="min-h-96">
         <DialogHeader>
           <DialogTitle>{getUser?.data?.name}</DialogTitle>
 
-          <ul>
-            <li>
-              <h5>Nome:</h5>
-              <p>{getUser?.data?.name}</p>
+          <ul className="w-full grid grid-cols-2 gap-5 p-10">
+            <li className="flex flex-col gap-1">
+              <h5 className="text-primary/80 font-semibold text-xl">Nome:</h5>
+              <p className="font-medium text-primary/70 text-lg">{getUser?.data?.name ?? '--'}</p>
             </li>
-            <li>
-              <h5>Email:</h5>
-              <p>{getUser?.data?.email}</p>
+            <li className="flex flex-col gap-1">
+              <h5 className="text-primary/80 font-semibold text-xl">Email:</h5>
+              <p className="font-medium text-primary/70 text-lg">{getUser?.data?.email ?? '--'}</p>
             </li>
-            <li>
-              <h5>Tipo:</h5>
-              <p>{getRoleText(getUser?.data?.role)}</p>
+            <li className="flex flex-col gap-1">
+              <h5 className="text-primary/80 font-semibold text-xl">Tipo:</h5>
+              <p className="font-medium text-primary/70 text-lg">{getRoleText(getUser?.data?.role) ?? '--'}</p>
             </li>
-            <li>
-              <h5>Criado em:</h5>
-              {/* FUNCTION */}
-              <p>{getUser?.data?.createdAt}</p>
+            <li className="flex flex-col gap-1">
+              <h5 className="text-primary/80 font-semibold text-xl">Criado em:</h5>
+              <p className="font-medium text-primary/70 text-lg">
+                {getUser?.data?.createdAt
+                  ? format(new Date(getUser.data.createdAt), "dd/MM/yyyy")
+                  : "--"}
+              </p>
             </li>
           </ul>
         </DialogHeader>
